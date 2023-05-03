3a5519a0554cea0fbe73c8f8a26ab139c21ab93b
Thallysson <thallyssonprogramerr14anos@gmail.com>
2023-05-03 15:22:48 -0300

    tela de criar conta

---
+++ src/views/SignUp/SignUp.vue
@@ -0,0 +1,15 @@
+
+
+<script>
+
+
+</script>
+
+<template>
+    <div class="home">
+        <h1>Criar Conta Garden Steakhouse</h1>
+        <router-link to="/minhasReservas">Minhas Reservas</router-link>
+    </div>
+</template>
+
+<style lang="scss" src="./style.scss" scoped/>
