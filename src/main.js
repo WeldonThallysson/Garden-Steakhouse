5d72d146950294cddcc5023824c37e29bb945d40
Thallysson <thallyssonprogramerr14anos@gmail.com>
2023-05-03 15:23:35 -0300

    arquivos principais configurados

--- src/main.js
+++ src/main.js
@@ -1,6 +1,40 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import { createApp } from 'vue'
+
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home/Home.vue'
+import MinhasReservas from './views/MinhasReservas/MinhasReservas.vue';
+import SignIn from './views/SignIn/SignIn.vue';
+import SignUp from './views/SignUp/SignUp.vue';
+
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path:'/',
+            name: 'singin',
+            component: SignIn,
+        },
+        {
+         path: '/signup',
+         name: 'signup',
+         component: SignUp,
+
+        },
+        { 
+          path: '/home',
+          name: 'home',
+          component: Home
+        
+        },
+        {
+            path:'/minhasReservas',
+            name: 'minhasreservas',
+            component: MinhasReservas,
+        }
+    ],
+})
+createApp(App).use(router).mount('#app')
