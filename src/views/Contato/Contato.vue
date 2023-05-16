5da774d8653ff333de204fe2a315c9a8bcdd26e0
Thallysson <thallyssonprogramerr14anos@gmail.com>
2023-05-16 13:19:13 -0300

    alteração no contato

--- src/views/Contato/Contato.vue
+++ src/views/Contato/Contato.vue
@@ -28,9 +28,10 @@ export default {
 
 
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14192.333056297495!2d-49.6465272!3d-27.2165355!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dfb906b5fa64eb%3A0xca52fcba8e59225f!2sDigiliza!5e0!3m2!1spt-PT!2sbr!4v1683344528630!5m2!1spt-PT!2sbr"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d467692.3958404301!2d-46.92497434052418!3d-23.68143453128611!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce448183a461d1%3A0x9ba94b08ff335bae!2zU8OjbyBQYXVsbywgU1A!5e0!3m2!1spt-PT!2sbr!4v1684253286781!5m2!1spt-PT!2sbr"
                             title="YouTube video" allowfullscreen style="border-radius: 11px" loading="lazy"
                             referrerpolicy="no-referrer-when-downgrade"></iframe>
+                          
                     </div>
                     <div>
                         <h4 class="titulo__localizacao">Siga nas Redes Sociais</h4>
